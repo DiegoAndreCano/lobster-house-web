ebe8fb4228190a8184aa3d1233a530e858f5a45d
DiegoAndreCano <69088380+DiegoAndreCano@users.noreply.github.com>
2020-09-05 04:49:53 -0500

    Beta v3

--- js/script.js
+++ js/script.js
@@ -8,14 +8,4 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var headerM = document.querySelector("#banner nav");
     headerM.classList.toggle("movil-nav");
   });
-
-  function switchStyle() {
-    if (document.getElementById("styleSwitch").checked) {
-      document.getElementById("gallery").classList.add("custom");
-      document.getElementById("exampleModal").classList.add("custom");
-    } else {
-      document.getElementById("gallery").classList.remove("custom");
-      document.getElementById("exampleModal").classList.remove("custom");
-    }
-  }
 });
